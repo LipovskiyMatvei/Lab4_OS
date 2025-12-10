867c29ea8bd0f01f5d984bdcd26f1eaa79f3465a
Lord of PC <lipov1962@gmail.com>
2025-12-11 00:33:02 +0300

    Lab4_OS_tests

---
+++ Lab4_OS_test/Lab4_OS_test.cpp
@@ -0,0 +1,34 @@
+﻿
+#define CATCH_CONFIG_MAIN 
+#include "catch.hpp"
+#include "Common.h"
+
+
+TEST_CASE("Constants and Structures check", "[setup]") {
+
+    REQUIRE(MSG_SIZE == 20);
+    REQUIRE(sizeof(QueueHeader) == 16); 
+}
+
+TEST_CASE("Offset Calculation", "[math]") {
+    int headerSize = sizeof(QueueHeader);
+
+    SECTION("First element offset") {
+        REQUIRE(getOffset(0) == headerSize);
+    }
+
+    SECTION("Second element offset") {
+        REQUIRE(getOffset(1) == headerSize + 20);
+    }
+}
+
+
+TEST_CASE("Circular Buffer Logic", "[logic]") {
+    int max_size = 5;
+    int tail = 4;
+
+
+    int next = (tail + 1) % max_size;
+
+    REQUIRE(next == 0);
+}
